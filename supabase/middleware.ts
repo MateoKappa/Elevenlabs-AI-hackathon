ea0907131ac9b4ebddb918970780ee1fa94447f8
Mateo Kappa <kapllanimateo01@gmail.com>
2025-02-23 13:19:29 +0200

    Update middleware.ts

--- supabase/middleware.ts
+++ supabase/middleware.ts
@@ -42,7 +42,6 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth") &&
     !request.nextUrl.pathname.startsWith("/api/auth")
   ) {
     // no user, potentially respond by redirecting the user to the login page
